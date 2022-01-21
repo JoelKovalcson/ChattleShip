afbf5f3040801a0c1e8aa66955e252c2da9eb72e
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-01-21 14:40:43 -0600

    added board routes

--- controllers/api/board-routes.js
+++ controllers/api/board-routes.js
@@ -1,15 +1,57 @@
 const router = require('express').Router();
+const { Board } = require('../../models')
 
 router.post('/', (req, res) => {
-
+    Board.create({
+        owner: req.body.owner,
+        grid: req.body.grid
+    })
+    .then(boardData => res.json(boardData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-
+    Board.update({
+        grid: req.body.grid
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(boardData => {
+        if (!boardData) {
+            res.status(404).json({message: 'No board with that id'});
+            return;
+        }
+        res.json(boardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-
+    Board.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(boardData => {
+        if (!boardData) {
+            res.status(404).json({message: 'No board with that id'});
+            return;
+        }
+        res.json(boardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
