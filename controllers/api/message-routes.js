bfb56990685f67b839a1e102641a5c48fc8dd724
Jordan Johnston <1086jjohnston@gmail.com>
2022-01-25 12:22:09 -0600

    added message routes

--- controllers/api/message-routes.js
+++ controllers/api/message-routes.js
@@ -1,11 +1,44 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Message } = require("../../models");
 
-router.get('/', (req, res) => {
-	console.log(req.query);
+router.get("/:id", (req, res) => {
+  Message.findAll({
+    where: {
+      $or: [
+        {
+          to: req.session.user_id,
+          from: req.params.id,
+        },
+        {
+          to: req.params.id,
+          from: req.session.user_id,
+        },
+      ],
+    },
+	limit: 20,
+	order: [
+		['created_at', 'DESC']
+	]
+  })
+    .then((dbMessageData) => res.json(dbMessageData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-
-});
+router.post( "/", /*withAuth,*/ (req, res) => {
+    Message.create({
+      message: req.body.message,
+      from: req.session.user_id,
+      to: req.body.to,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+);
 
 module.exports = router;
