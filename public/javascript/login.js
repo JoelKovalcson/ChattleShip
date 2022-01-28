505f32846a163e3dfe36b81eb0e9cef313c20b92
Joel Kovalcson <joelkovalcson@gmail.com>
2022-01-27 20:22:58 -0500

    logout functionality partially done

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,52 +1,55 @@
 async function loginFormHandler(event) {
 	event.preventDefault();
-  
+
 	const username = document.querySelector('#username-login').value.trim();
 	const password = document.querySelector('#password-login').value.trim();
-  
+
 	if (username && password) {
-	  const response = await fetch('/api/user/login', {
-		method: 'post',
-		body: JSON.stringify({
-		  user_name: username,
-		  password
-		}),
-		headers: { 'Content-Type': 'application/json' }
-	  });
-  
-	  if (response.ok) {
-		document.location.replace('/dashboard/');
-	  } else {
-		alert(response.statusText);
-	  }
+		const response = await fetch('/api/user/login', {
+			method: 'post',
+			body: JSON.stringify({
+				user_name: username,
+				password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard/');
+		} else {
+			alert(response.statusText);
+		}
 	}
-  }
-  
-  async function signupFormHandler(event) {
+}
+
+async function signupFormHandler(event) {
 	event.preventDefault();
-  
+
 	const username = document.querySelector('#username-signup').value.trim();
 	const password = document.querySelector('#password-signup').value.trim();
-  
+
 	if (username && password) {
-	  const response = await fetch('/api/user', {
-		method: 'post',
-		body: JSON.stringify({
-		  user_name: username,
-		  password
-		}),
-		headers: { 'Content-Type': 'application/json' }
-	  });
-  
-	  if (response.ok) {
-		document.location.replace('/dashboard/');
-	  } else {
-		alert(response.statusText);
-	  }
+		const response = await fetch('/api/user', {
+			method: 'post',
+			body: JSON.stringify({
+				user_name: username,
+				password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard/');
+		} else {
+			alert(response.statusText);
+		}
 	}
-  }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
