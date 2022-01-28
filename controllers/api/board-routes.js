d655a1e50fc3508ebc90c4922a10041a70749d8d
Joel Kovalcson <joelkovalcson@gmail.com>
2022-01-28 18:08:07 -0500

    Updated Board+Game routes for new models

--- controllers/api/board-routes.js
+++ controllers/api/board-routes.js
@@ -1,57 +1,63 @@
 const router = require('express').Router();
-const { Board } = require('../../models');
+const {
+	Board
+} = require('../../models');
 
 router.post('/', (req, res) => {
-    Board.create({
-        owner: req.body.owner,
-        grid: req.body.grid
-    })
-    .then(boardData => res.json(boardData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Board.create({
+			owner: req.session.user_id,
+			grid: req.body.grid,
+			game_id: req.body.game_id
+		})
+		.then(boardData => res.json(boardData))
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.put('/:id', (req, res) => {
-    Board.update({
-        grid: req.body.grid
-    },
-    {
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(boardData => {
-        if (!boardData) {
-            res.status(404).json({message: 'No board with that id'});
-            return;
-        }
-        res.json(boardData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Board.update({
+			grid: req.body.grid
+		}, {
+			where: {
+				id: req.params.id
+			}
+		})
+		.then(boardData => {
+			if (!boardData) {
+				res.status(404).json({
+					message: 'No board with that id'
+				});
+				return;
+			}
+			res.json(boardData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.delete('/:id', (req, res) => {
-    Board.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(boardData => {
-        if (!boardData) {
-            res.status(404).json({message: 'No board with that id'});
-            return;
-        }
-        res.json(boardData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Board.destroy({
+			where: {
+				id: req.params.id
+			}
+		})
+		.then(boardData => {
+			if (!boardData) {
+				res.status(404).json({
+					message: 'No board with that id'
+				});
+				return;
+			}
+			res.json(boardData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
