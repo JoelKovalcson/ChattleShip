cb8041b7043e201462c06294bf6d796f855db716
Joel Kovalcson <joelkovalcson@gmail.com>
2022-01-27 21:48:26 -0500

    Added game routes and added username to page renders

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
 	if (req.session.loggedIn) {
-		res.redirect('/dashboard', {
-			loggedIn: req.session.loggedIn
-		});
+		res.redirect('/dashboard');
 		return;
 	}
 	res.render('login');
@@ -13,21 +11,24 @@ router.get('/', (req, res) => {
 router.get('/dashboard', (req, res) => {
 	// TODO: Add game and chat information
 	res.render('dashboard', {
-		loggedIn: req.session.loggedIn
+		loggedIn: req.session.loggedIn,
+		user_name: req.session.user_name
 	});
 });
 
 router.get('/game', (req, res) => {
 	// TODO: Add game and chat information
 	res.render('game', {
-		loggedIn: req.session.loggedIn
+		loggedIn: req.session.loggedIn,
+		user_name: req.session.user_name
 	});
 });
 
 router.get('/profile', (req, res) => {
 	// TODO: Add profile and chat information
 	res.render('profile', {
-		loggedIn: req.session.loggedIn
+		loggedIn: req.session.loggedIn,
+		user_name: req.session.user_name
 	});
 });
 
