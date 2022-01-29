fe0d946a177629792c1a8a289cb27671df9a3028
Joel Kovalcson <joelkovalcson@gmail.com>
2022-01-28 22:58:28 -0500

    Added game and board creation files

---
+++ public/javascript/make-board.js
@@ -0,0 +1,42 @@
+var newGrid = {
+	ships: [
+		{
+			row: null,
+			col: null,
+			length: 2,
+			isSank: false,
+			isHorizontal: true
+		},
+		{
+			row: null,
+			col: null,
+			length: 3,
+			isSank: false,
+			isHorizontal: true
+		},
+		{
+			row: null,
+			col: null,
+			length: 3,
+			isSank: false,
+			isHorizontal: true
+		},
+		{
+			row: null,
+			col: null,
+			length: 4,
+			isSank: false,
+			isHorizontal: true
+		},
+		{
+			row: null,
+			col: null,
+			length: 5,
+			isSank: false,
+			isHorizontal: true
+		}
+	],
+	myGrid: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	enemyGrid: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+}
+
