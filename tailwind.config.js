5354fe66dc97c6c442d339f0481784a05ded00a9
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-01-29 18:02:17 -0600

    added tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ const tailwindcss = require("tailwindcss");
 module.exports = {
   content: [
    './views/**/*.handlebars',
-   './public/**/*.js'
+   './public/**/*.js',
+   './src/**/*.{html,js}', 
+   './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('tw-elements/dist/plugin')
   ],
 }
